928134d6105d24e1deaae11278048f5a16e195e3
Fatihbykl <fatih.baykal54@hotmail.com>
2024-08-27 23:39:02 +0300

    Initialized

---
+++ migrations/00002_create_books_table.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+-- +goose StatementEnd
+
+CREATE TABLE IF NOT EXISTS books
+(
+    id             UUID PRIMARY KEY,
+    title          TEXT      NOT NULL,
+    author         TEXT      NOT NULL,
+    published_date DATE      NOT NULL,
+    image_url      TEXT      NULL,
+    description    TEXT      NULL,
+    created_at     TIMESTAMP NOT NULL,
+    updated_at     TIMESTAMP NOT NULL,
+    deleted_at     TIMESTAMP NULL
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS books;
+-- +goose StatementBegin
+SELECT 'down SQL query';
+-- +goose StatementEnd
