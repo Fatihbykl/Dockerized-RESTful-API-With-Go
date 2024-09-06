a974481faaa073a2ea53a39afd5404576da4f573
Fatihbykl <fatih.baykal54@hotmail.com>
2024-09-06 22:19:41 +0300

    Added routes and OpenAPI specifications

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,22 +2,27 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 
+	"myapp/api/router"
 	"myapp/config"
 )
 
+//  @title          MYAPP API
+//  @version        1.0
+//  @description    This is a sample RESTful API with a CRUD
+
+//  @contact.name   Fatih
+
+// @host       localhost:8080
+// @basePath   /v1
 func main() {
 	c := config.New()
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", hello)
-
+	r := router.New()
 	s := &http.Server{
 		Addr:         fmt.Sprintf(":%d", c.Server.Port),
-		Handler:      mux,
+		Handler:      r,
 		ReadTimeout:  c.Server.TimeoutRead,
 		WriteTimeout: c.Server.TimeoutWrite,
 		IdleTimeout:  c.Server.TimeoutIdle,
@@ -27,8 +32,4 @@ func main() {
 	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatal("Server startup failed")
 	}
-}
-
-func hello(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello World!")
 }
